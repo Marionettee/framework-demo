9116aad61f6a14a93ea6b3f5cd47761277bc2b0b
Haonan <330764949@qq.com>
2019-05-21 17:01:05 +0800

    add promise ES5 code

--- script/promise.js
+++ script/promise.js
@@ -1,3 +1,4 @@
+// ES6
 class demoPromise{
     constructor(fn){
         this.state = 'pending';
@@ -49,3 +50,57 @@ class demoPromise{
         },0)
     }
 }
+
+// ES5
+var PromiseES5 = (function(){
+    function promise(fn){
+        this.state = 'pending';
+        this.callback={};
+        fn(this.resolve.bind(this),this.reject.bind(this));
+    }
+    promise.prototype.then = function(fulfilled,rejected){
+        if(this.state === 'pending'){
+            this.callback.fulfilled = fulfilled;
+            if(rejected){
+                this.callback.reject = rejected;
+            }
+        }
+        return this
+    };
+    promise.prototype.catch=function(error){
+        if(this.state === 'pending'){
+            this.callback.rejected = error;
+        }
+        return this;
+    };
+    promise.prototype.finally=function(fn){
+        this.callback.finally = fn;
+    };
+    promise.prototype.resolve=function(val){
+        setTimeout(()=>{
+            if(this.callback.fulfilled){
+                this.state = 'fulfilled';
+                this.callback.fulfilled(val);
+                if(this.callback.finally){
+                    this.callback.finally();
+                }
+            }else{
+                console.error('缺少回调');            
+            }
+        },0)
+    };
+    promise.prototype.reject=function(val){
+        setTimeout(()=>{
+            if(this.callback.rejected){
+                this.state = 'rejected';
+                this.callback.reject(val);
+                if(this.callback.finally){
+                    this.callback.finally();
+                }
+            }else{
+                console.error('缺少回调');
+            }
+        },0)
+    };
+    return promise;
+})()
